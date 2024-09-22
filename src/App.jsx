7ccd8c75827ce76a7f81f8ba8a75ee8f42d61f52
gairik <gairikgmandal@gmail.com>
2024-09-22 11:30:27 +0530

    usestate for inputField is dropped

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { v4 as uuid } from 'uuid'
 import './App.css'
 
 function App() {
+  // let input = document.querySelector('.input');
+  // console.log(input.value);
 
-  const [todo, setTodo] = useState('');
+  // const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
@@ -17,14 +19,15 @@ function App() {
     localStorage.setItem('todoList', JSON.stringify(todoList));
   }, [todoList]);
 
-  let getTodo = (e) => {
-    setTodo(e.target.value);
-  };
+  // let getTodo = (e) => {
+  //   setTodo(e.target.value);
+  // };
 
   let getTodoList = () => {
-    if (todo.length > 0)
-      setTodoList([...todoList, { id: uuid(), todo: todo, isChecked: false }]);
-    setTodo('');
+    let input = document.querySelector('input');
+    if (input.value.length > 0)
+      setTodoList([...todoList, { id: uuid(), todo: input.value, isChecked: false }]);
+    input.value = '';
   };
 
   let del = (e) => {
@@ -44,19 +47,19 @@ function App() {
     <>
       <div className='App'>
         <div>
-          <input type="text" value={todo} placeholder='Add List Item' onChange={getTodo} className='p-3 my-3 input' />
+          <input type="text" placeholder='Add List Item' className='p-3 my-3 input' />
           <button onClick={getTodoList} className='btn btn-primary btn-sm p-3 mx-2 mb-1'>Add+</button>
         </div>
         <div className='my-5'>
           {
             todoList.map(({ id, todo, isChecked }) => (
-              <div data-id={id} className='row  justify-content-center my-3 p-0'>
-                <div className='col-1 my-auto text-start'>
+              <div key={id} className='row  justify-content-center my-3 p-0'>
+                <div className='col-1 my-auto text-start' data-id={id}>
                   <input type="checkbox" data-id={id} checked={isChecked} onChange={isCheck} className='form-check-input' />
                 </div>
 
-                <p className={`${isChecked ? 'strike' : ''} col-5 text-light text-start word-break my-auto`}>{todo}</p>
-                <button onClick={del} data-id={id} className='btn col-1 '><span data-id={id} class="material-symbols-outlined text-light">
+                <p className={`${isChecked ? 'strike' : ''} col-5 text-light text-start word-break my-auto`} data-id={id}>{todo}</p>
+                <button onClick={del} data-id={id} className='btn col-1 '><span data-id={id} className="material-symbols-outlined text-light">
                   delete
                 </span></button>
               </div>
